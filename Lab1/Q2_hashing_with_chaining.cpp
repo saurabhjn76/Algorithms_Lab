1ef3caf2af403e79161bb9e34d2c5530825e7718
saurabhjn76 <saurabhjn76@gmail.com>
2016-01-18 14:29:22 +0530

    Added question2 code

---
+++ Lab1/Q2_hashing_with_chaining.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h> 
+using namespace std;
+# define SIZE 10
+typedef struct hash_table{
+	int value;
+	struct hash_table *next;
+}hash_table;
+hash_table table[SIZE];
+hash_table* makeNode(int k)
+{
+	hash_table *node=(hash_table*)calloc(1,sizeof(hash_table));
+	node->value=k;
+	node->next=NULL;
+	return node;
+}
+void insert(int k)
+{
+	int key=k%SIZE;
+	if(table[key].value==-1)
+		table[key].value=k;
+	else
+	{
+		hash_table *temp=&(table[key]);
+
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+		}
+		temp->next=makeNode(k);
+	}
+
+}
+
+void display()
+{
+	hash_table * temp;
+	for(int i=0;i<SIZE;i++)
+	{
+		if(table[i].value!=-1)
+		{
+			for(temp=&(table[i]);temp!=NULL;temp=temp->next)
+				printf("%d-->",temp->value);
+			printf("end\n");
+		}
+	}
+
+}
+int search(int k)
+{
+	hash_table * temp;
+	int key=k%SIZE;
+	if(table[key].value!=-1)
+		{
+			for(temp=&(table[key]);temp!=NULL ;temp=temp->next)
+				if(temp->value==k)
+				{
+					printf("%d-->Found\n",temp->value);
+					return 1;
+					break;
+				}
+		}
+	else
+		{
+			printf("The element is not present\n");
+			return 0;
+		}
+	if(temp==NULL)
+		{
+			printf("The element is not present\n");
+			return 0;
+		}
+}
+int deleted(int k)
+{
+	hash_table * temp,*prev;
+	int key=k%SIZE;
+	if(table[key].value!=-1)
+		{
+			for(temp=&(table[key]);temp!=NULL ;temp=temp->next)
+			{
+				
+					if(temp->value==k)
+				{
+					prev->next=temp->next;
+					free(temp);
+					return 1;
+					break;
+				}
+				prev=temp;
+			}
+		}
+	else
+		{
+			printf("The element is not present\n");
+			return 0;
+		}
+	if(temp==NULL)
+		{
+			printf("The element is not present\n");
+			return 0;
+		}
+
+}
+int main()
+{
+	int i,j,k,ch=0;
+	for(i=0;i<SIZE;i++)
+		table[i].value=-1;
+	while(ch!=5)
+	{
+		printf("Choose\n1)insert 2) search 3) delete 4)display 5)EXIT\n");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1: printf("Enter the element\n");
+					scanf("%d",&k);
+					insert(k);
+					break;
+			case 2: printf("Enter the element\n");
+					scanf("%d",&k);
+					search(k);
+					break;
+			case 3: printf("Enter the element\n");
+					scanf("%d",&k);
+					deleted(k);
+					break;
+			case 4: display();
+					break;
+		}
+	}
+
+	return 0;
+}
---
+++ Lab1/q2stack.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+	// stack using 2 queues
+int l1=0,l2=0;
+int size=10;
+int q1[10],q2[10];
+int frontA=0,frontB=0,rearA=-1,rearB=-1;
+bool notEmpty(int i)
+{
+	if(i==1)
+	{
+		if(frontA>rearA)
+			return false;
+		else
+			return true;
+	}
+	else
+		if(frontB>rearB)
+			return false;
+		else
+			return true;
+}
+void enqueue(int i,int element)
+{
+	if(i==1)
+	{
+		q1[++rearA%size]=element;
+
+	}
+	else 
+	{
+		q2[++rearB%size]=element;
+	}
+}
+int dequeue(int i)
+{
+	if(i==1)
+	{
+		return q1[frontA++%size];
+
+	}
+	else
+		return q2[frontB++%size];
+}
+
+void push(int k){
+	if(rearA-frontA<size)
+	{
+		if(rearA==-1)
+			enqueue(1,k);
+		else
+		{
+			while(notEmpty(1))
+			{
+				enqueue(2,dequeue(1));
+
+			}
+			enqueue(1,k);
+			while(notEmpty(2))
+			{
+				enqueue(1,dequeue(2));
+
+			}
+
+		}
+	}
+	else
+		printf("The queue is full\n");
+}
+void pop(){
+	if(!notEmpty(1))
+		printf("The stack is empty\n");
+	else
+	{
+		dequeue(1);
+
+	}
+
+}
+void display()
+{
+	int i;
+	for(i=frontA;i<=rearA;i++)
+	{
+		printf("%d-->",q1[i]);
+	}
+	printf("end\n");
+
+}
+int main()
+{
+	
+	int i,j=0,k,l;
+	do{
+	printf("Choose\n1)Push 2) Pop 3) display 4) exit\n");
+	scanf("%d",&i);
+	switch(i)
+	{
+		case 1: printf("Enter the element\n");
+				scanf("%d",&k);
+				push(k);
+				break;
+		case 2: pop();
+				break;
+		case 3: display();
+				break;
+		case 4: j=1;
+				break;
+		default: printf("Wrong choice\n");
+	}
+
+	}while(j==0);
+	return 0;
+}
